d6061f6ee57db5972492a9af06b3c916e0ced961
Danang Priyombodo <32449624+overboxbgrff@users.noreply.github.com>
2024-05-12 13:03:06 +0800

    Add files via upload
    Initial upload

---
+++ MenuRes.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class MenuRes {
+
+    public static String[] makanan = {
+            "Nasi Goreng",
+            "Bakso",
+            "Batagor",
+            "Mie Ayam",
+            "Soto Betawi"
+    };
+    public static int[] hargamakan = {
+            20000,
+            22000,
+            15000,
+            18000,
+            25000
+    };
+    public static String[] minuman = {
+            "Jus Jeruk",
+            "Jus Alpukat",
+            "Es Teh",
+            "Kopi Panas"
+    };
+    public static int[] hargaminum = {
+            5000,
+            7000,
+            5000,
+            5000
+    };
+}
+
